4d481051956179b0f4c07961daebf9da3ef47d96
rrgodhorus <rajath@github.com>
2017-12-21 20:26:25 +0530

    Initial commit

---
+++ app/src/main/java/com/rajath/reminderapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.rajath.reminderapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+     android.support.v7.widget.Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (android.support.v7.widget.Toolbar)findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
