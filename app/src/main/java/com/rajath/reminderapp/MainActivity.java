846c156daf96270498a2676956ed3e0310a32c18
rrgodhorus <rajath@github.com>
2017-12-27 21:36:56 +0530

    Added toolbar,logo,search logo and launcher logo

--- app/src/main/java/com/rajath/reminderapp/MainActivity.java
+++ app/src/main/java/com/rajath/reminderapp/MainActivity.java
@@ -2,20 +2,47 @@ package com.rajath.reminderapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
-     android.support.v7.widget.Toolbar toolbar;
+     Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = (android.support.v7.widget.Toolbar)findViewById(R.id.my_toolbar);
+        toolbar = findViewById(R.id.my_toolbar);
+        toolbar.setLogo(R.drawable.ic_launcher);
         setSupportActionBar(toolbar);
-        //Its working right?
     }
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        }
 
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_favorite) {
+                Toast.makeText(MainActivity.this, "Action clicked", Toast.LENGTH_LONG).show();
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+
+
+        }
 
 
 
